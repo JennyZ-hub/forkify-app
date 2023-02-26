a8718f57d557eab3687a33a7b46fd91d2ee4b6a4
Jenny Zeng <420337484@qq.com>
2023-02-26 15:06:41 +1100

    refactoring by using MVC

---
+++ src/js/model.js
@@ -0,0 +1,27 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message}(${res.status})`);
+    let { recipe } = data.data;
+    state.recipe = {
+      cookingTime: recipe.cooking_time,
+      id: recipe.id,
+      image: recipe.image_url,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      title: recipe.title,
+      sourceUrl: recipe.source_url,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
